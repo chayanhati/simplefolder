ef14e99e4011ea3d4c7f5e62cf3197a8df7e495a
Chayan-07 <135729690+chayanhati@users.noreply.github.com>
2026-02-19 21:35:42 +0530

    Update restaurant_menu.js

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -2,6 +2,10 @@ const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
 const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
 const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
 
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) =>
+    `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
 let mainCourseItem = '';
 mainCourseMenu.forEach((item, index) => {
     mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;
@@ -13,6 +17,3 @@ for (let i = 0; i < dessertMenu.length; i++) {
     dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;
 }
 document.getElementById('dessertMenuItems').innerHTML = dessertItem;
-
-const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
-document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
